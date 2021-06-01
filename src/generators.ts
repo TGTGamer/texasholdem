bfba991a8c55bde86d055b3c481688f1e0a286ff
Jonathan S <tgtgam3r@gmail.com>
2021-06-01 18:09:44 +0100

    update generators.ts

--- src/generators.ts
+++ src/generators.ts
@@ -44,14 +44,21 @@ export async function dealHand(deck: Deck, cards: number) {
 export async function createTable (deck:Deck, players: number): Promise<Players> {
     let result: Partial<Players> = {}
     let cardsDealt: number = 0
+    
+    // deal cards to players
     while (cardsDealt) {
         let playersDealt: number = 0
-        while (playersDealt !== players+1) {
-            if (playersDealt == players) result.table = dealHand(deck,1)
-            else result[playersDealt] = dealHand(deck, 1)
+        while (playersDealt !== players) {
+            result[playersDealt] = dealHand(deck, 1)
             playersDealt++
         }
         cardsDealt++
     }
+    
+    // burn card
+    result.burn = dealHand(deck, 1)
+    
+    // the flop
+    result.table = dealHand(deck, 3)
     return result
 }
