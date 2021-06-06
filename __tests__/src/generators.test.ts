475cbc88fd0f9a6a806798a801db214a0b61321b
Jonathan Stevens <jonathan@videndum.studio>
2021-06-06 13:27:15 +0100

    test(generators): rebuild and create tests for generators using Game class

--- __tests__/src/generators.test.ts
+++ __tests__/src/generators.test.ts
@@ -3,54 +3,101 @@ import * as index from '../../src/index'
 
 // @ponicode
 describe('generators.generateDeck', () => {
-  test('0', async () => {
-    let result: any = await generators.generateDeck(1)
-    expect(result.shuffled).toBe(undefined)
-    expect(result.decks).toBe(1)
-    expect(result.cards.length).toBe(56)
+  let inst: index.Game
+
+  beforeEach(() => {
+    inst = new index.Game()
   })
 
-  test('2', async () => {
-    let result: any = await generators.generateDeck(5)
-    expect(result.shuffled).toBe(undefined)
-    expect(result.decks).toBe(5)
-    expect(result.cards.length).toBe(280)
+  test('0', async () => {
+    let result: any = await generators.generateDeck.bind(inst)(1)
+    expect(result).toBe(true)
+    let deck: any = await inst.getDeck()
+    expect(deck.shuffled).toBe(undefined)
+    expect(deck.decks).toBe(1)
+    expect(deck.cards.length).toBe(56)
   })
 
   test('1', async () => {
-    let result: any = await generators.generateDeck(2)
-    expect(result.shuffled).toBe(undefined)
-    expect(result.decks).toBe(2)
-    expect(result.cards.length).toBe(112)
+    let result: any = await generators.generateDeck.bind(inst)(2)
+    expect(result).toBe(true)
+    let deck: any = await inst.getDeck()
+    expect(deck.shuffled).toBe(undefined)
+    expect(deck.decks).toBe(2)
+    expect(deck.cards.length).toBe(112)
+  })
+
+  test('2', async () => {
+    let result: any = await generators.generateDeck.bind(inst)(5)
+    expect(result).toBe(true)
+    let deck: any = await inst.getDeck()
+    expect(deck.shuffled).toBe(undefined)
+    expect(deck.decks).toBe(5)
+    expect(deck.cards.length).toBe(280)
   })
 
   test('3', async () => {
-    let result: any = await generators.generateDeck(3)
-    expect(result.shuffled).toBe(undefined)
-    expect(result.decks).toBe(3)
-    expect(result.cards.length).toBe(168)
+    let result: any = await generators.generateDeck.bind(inst)(3)
+    expect(result).toBe(true)
+    let deck: any = await inst.getDeck()
+    expect(deck.shuffled).toBe(undefined)
+    expect(deck.decks).toBe(3)
+    expect(deck.cards.length).toBe(168)
   })
 
   test('4', async () => {
-    let result: any = await generators.generateDeck(-Infinity)
-    expect(result.shuffled).toBe(undefined)
-    expect(result.decks).toBe(-Infinity)
-    expect(result.cards.length).toBe(0)
+    let result: any = await generators.generateDeck.bind(inst)(-Infinity)
+    expect(result).toBe(true)
+    let deck: any = await inst.getDeck()
+    expect(deck.shuffled).toBe(undefined)
+    expect(deck.decks).toBe(-Infinity)
+    expect(deck.cards.length).toBe(0)
   })
 })
 
 // @ponicode
 describe('generators.shuffleDeck', () => {
+  let inst: index.Game
+
+  beforeEach(() => {
+    inst = new index.Game()
+  })
   test('1', async () => {
-    let object = [
-      { set: index.CardSet.clubs, face: index.Faces.four },
-      { set: index.CardSet.spades, face: index.Faces.King },
-      { set: index.CardSet.diamonds, face: index.Faces.nine }
-    ]
-    let result = await generators.shuffleDeck(
-      { cards: object, decks: 1, shuffled: undefined },
-      2
-    )
+    let result = await generators.shuffleDeck.bind(inst)({ count: 2 })
     expect(result.shuffled.times).toBe(2)
+  })
+})
+
+// @ponicode
+describe('generators.splitDeck', () => {
+  let inst: index.Game
+  let object = [
+    { set: index.CardSet.clubs, face: index.Faces.four },
+    { set: index.CardSet.clubs, face: index.Faces.four },
+    { set: index.CardSet.clubs, face: index.Faces.four },
+    { set: index.CardSet.spades, face: index.Faces.King },
+    { set: index.CardSet.spades, face: index.Faces.King },
+    { set: index.CardSet.spades, face: index.Faces.King },
+    { set: index.CardSet.diamonds, face: index.Faces.nine },
+    { set: index.CardSet.diamonds, face: index.Faces.nine },
+    { set: index.CardSet.diamonds, face: index.Faces.nine }
+  ]
+
+  beforeEach(async () => {
+    inst = new index.Game()
+  })
+  test('1', async () => {
+    let result = await generators.splitDeck.bind(inst)({
+      skipValidate: true,
+      deck: {
+        cards: object,
+        decks: 1,
+        shuffled: {
+          times: 2,
+          split: false
+        }
+      }
+    })
+    console.log(result)
   })
 })
